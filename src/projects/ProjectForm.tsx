d0ce30d42d9ff76c2c0368e82e32dc83196814c0
sarasingh <ssingh@smartfutures.org>
2025-03-10 11:04:33 +0530

    lab6

--- src/projects/ProjectForm.tsx
+++ src/projects/ProjectForm.tsx
@@ -89,31 +89,36 @@ function ProjectForm({ project: initialProject, onCancel }: ProjectFormProps) {
       <input
         type="text"
         name="name"
+        id="name"
+        aria-label="project name"
         placeholder="enter name"
         value={project.name}
         onChange={handleChange}
       />
       {errors.name.length > 0 && (
-        <div className="card error">
+        <div role="alert" className="card error">
           <p>{errors.name}</p>
         </div>
       )}
 
       <label htmlFor="description">Project Description</label>
       <textarea
+        id="description"
+        aria-label="project description"
         name="description"
         placeholder="enter description"
         value={project.description}
         onChange={handleChange}
       />
       {errors.description.length > 0 && (
-        <div className="card error">
+        <div role="alert" className="card error">
           <p>{errors.description}</p>
         </div>
       )}
 
       <label htmlFor="budget">Project Budget</label>
       <input
+        id="budget"
         type="number"
         name="budget"
         placeholder="enter budget"
@@ -121,13 +126,14 @@ function ProjectForm({ project: initialProject, onCancel }: ProjectFormProps) {
         onChange={handleChange}
       />
       {errors.budget.length > 0 && (
-        <div className="card error">
+        <div role="alert" className="card error">
           <p>{errors.budget}</p>
         </div>
       )}
 
       <label htmlFor="isActive">Active?</label>
       <input
+        id="isActive"
         type="checkbox"
         name="isActive"
         checked={project.isActive}
